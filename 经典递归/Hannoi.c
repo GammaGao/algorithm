3c6947595c3c3d2dd70dd565ab06e978c38ed7ee
Joe.P <psc0606@users.noreply.github.com>
2017-03-29 19:17:47 +0800

    Update Hannoi.c
    add comment

--- 经典递归/Hannoi.c
+++ 经典递归/Hannoi.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+/**
+ * Hanoi Tower
+ * @date 2017.03.29
+ */
 void hannoi(int n, char a, char b, char c)
 {
 	if(1 == n)
-		printf("Move disk %d from %c to %c\n", n, a, c);
+		printf("Move disk-%d from %c to %c\n", n, a, c);
 	else{
+		//move the n-1 disks above from A to B. So the max disk-n is on the A.
 		hannoi(n-1, a, c, b);
-		printf("Move disk %d from %c to %c\n", n, a, c);
+		
+		//move the current max disk from A to C.
+		printf("Move disk-%d from %c to %c\n", n, a, c);
+		
+		//move the rest n-1 disks from B to A.
 		hannoi(n-1, b, a, c);
 	}
 		
